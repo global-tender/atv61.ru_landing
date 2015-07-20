56365d0ce70cea9ec38d5d58d3e622a7c8a97aca
Igor Popov <ihptru@gmail.com>
2015-07-20 16:04:49 +0000

    finished most blocks; presentation version

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
 		<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel="stylesheet" type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Lobster' rel="stylesheet" type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700&subset=latin,cyrillic-ext,cyrillic,latin-ext' rel='stylesheet' type='text/css'>
+		<link href="http://fonts.googleapis.com/css?family=PT+Sans:regular,italic,bold,bolditalic" rel="stylesheet" type="text/css" />
 	<!--//fonts-->
 		<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 	<!-- for-mobile-apps -->
@@ -40,6 +41,7 @@
 		<script type="text/javascript" src="js/move-top.js"></script>
 		<script type="text/javascript" src="js/easing.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/0.9.0/jquery.mask.min.js"></script>
+		<script type="text/javascript" src="js/jquery.jcarousel.js"></script>
 		<script type="text/javascript">
 			jQuery(document).ready(function($) {
 				$(".scroll").click(function(event){		
@@ -101,9 +103,9 @@
 					<nav class="cl-effect-1">
 						<ul class="nav1">
 							<li><a class="scroll" href="#home"><img src="images/home1-min.png" class="home-icon" /></a></li>
-							<li><a class="scroll" href="#about-us">О нас</a></li>
-							<li><a class="scroll" href="#pricing-table">Услуги</a></li>
-							<li><a class="scroll" href="#our-team">Преимущества</a></li>
+							<li><a class="scroll" href="#we-are-atv">О нас</a></li>
+							<li><a class="scroll" href="#atv-services">Услуги</a></li>
+							<li><a class="scroll" href="#atv-feedbacks">Отзывы</a></li>
 							<li><a class="scroll" href="#get-in-touch">Контакты</a></li>
 						</ul>
 					</nav>
@@ -142,14 +144,17 @@
 	</div>
 </div>
 <!-- //navigation -->
+
+<div class="bottle"></div>
+
 <!-- our services -->
-<div class="our-services">
+<div id="we-are-atv" class="our-services">
 	<div class="container">
 		<div class="our-info text-center">
-			<h2><span class="highlight">OUR</span> SERVICES</h2>
-			<p><i>What We Do With Love</i></p>
+			<h2><span class="highlight">Наша</span> компания</h2>
+			<p><i>Почему выбирают нас</i></p>
 			<div class="underline">
-				<img src="images/underline.png" alt=" "/>
+				
 			</div>
 		</div>
 		<div class="our-grids">
@@ -158,8 +163,8 @@
 					<div class="brand-image"></div>
 				</div>
 				<div class="col-md-9 our-grid-right">
-					<h3>Brand Identity</h3>
-					<p>aliquam ipsum ante morbi sed ipsum mollis. Sollicitudin viverra, vel varius eget sit mollis.</p>
+					<h3>18 лет опыта</h3>
+					<!-- <p>aliquam ipsum ante morbi sed ipsum mollis. Sollicitudin viverra, vel varius eget sit mollis.</p> -->
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -168,8 +173,9 @@
 					<div class="bulb-image"></div>
 				</div>
 				<div class="col-md-9 our-grid-right">
-					<h3>UI/UX Design</h3>
-					<p>aliquam ipsum ante morbi sed ipsum mollis. Sollicitudin viverra, vel varius eget sit mollis.</p>
+					<h3>Без затрат на бух.
+					программы и справочно-
+					правовые системы</h3>
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -178,8 +184,10 @@
 					<div class="graphic-image"></div>
 				</div>
 				<div class="col-md-9 our-grid-right">
-					<h3>Graphic Design</h3>
-					<p>aliquam ipsum ante morbi sed ipsum mollis. Sollicitudin viverra, vel varius eget sit mollis.</p>
+					<h3>Гарантированная 
+					правильность ведения
+					учёта и составления
+					отчётности</h3>
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -188,8 +196,8 @@
 					<div class="mobile-image"></div>
 				</div>
 				<div class="col-md-9 our-grid-right">
-					<h3>Mobile Application</h3>
-					<p>aliquam ipsum ante morbi sed ipsum mollis. Sollicitudin viverra, vel varius eget sit mollis.</p>
+					<h3>Фиксированный
+					размер оплаты</h3>
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -198,8 +206,8 @@
 					<div class="illustration-image"></div>
 				</div>
 				<div class="col-md-9 our-grid-right">
-					<h3>Illustration</h3>
-					<p>aliquam ipsum ante morbi sed ipsum mollis. Sollicitudin viverra, vel varius eget sit mollis.</p>
+					<h3>Мы не уходим в
+					отпуск и не болеем</h3>
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -208,8 +216,9 @@
 					<div class="video-image"></div>
 				</div>
 				<div class="col-md-9 our-grid-right">
-					<h3>Videography</h3>
-					<p>aliquam ipsum ante morbi sed ipsum mollis. Sollicitudin viverra, vel varius eget sit mollis.</p>
+					<h3>Без затрат на
+					организацию рабочего
+					места</h3>
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -225,23 +234,23 @@
 		<div class="bottom-grids">
 			<div class="col-md-3 bottom-grid text-center">
 				<div class="like"></div>
-				<h2>2006</h2>
-				<p>Satisfied Client</p>
+				<h2>10</h2>
+				<p>Лет опыта</p>
 			</div>
 			<div class="col-md-3 bottom-grid text-center">
 				<div class="chat"></div>
-				<h2>240</h2>
-				<p>Clients Served</p>
+				<h2>150</h2>
+				<p>Компаний уже доверяют ведение бух. учёта нам</p>
 			</div>
 			<div class="col-md-3 bottom-grid text-center">
 				<div class="smiley"></div>
-				<h2>120</h2>
-				<p>Projects Delivered</p>
+				<h2>5</h2>
+				<p>Квалифицированных бухгалтеров в штате</p>
 			</div>
 			<div class="col-md-3 bottom-grid text-center">
 				<div class="flag"></div>
-				<h2>10</h2>
-				<p>Awards Won</p>
+				<h2>&#8399;</h2>
+				<p>Фиксированный размер оплаты</p>
 			</div>
 			<div class="clearfix"></div>
 		</div>
@@ -251,66 +260,105 @@
 
 
 <!-- pricing-table -->
-<div id="pricing-table" class="pricing-table">
+<div id="atv-services" class="pricing-table">
 	<div class="container">
 		<div class="pricing-info text-center">
-			<h2><span class="highlight">PRICING</span> TABLE</h2>
-			<h3><i>Choose The Best For You</i></h3>
+			<h2><span class="highlight">Наши</span> услуги</h2>
+			<h3><i>Бухгалтерское сопровождение, Аудит и многое другое для Вашего успешного бизнеса</i></h3>
 			<div class="underline">
-				<img src="images/underline3.png" alt=" "/>
+				
 			</div>
-			<p>Lorem ipsum dolor amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi suspendisse</p>
 		</div>
 		<div class="pricing-grids">
 			<div class="pricing-grid">
 				<div class="basic-plan">
-					<h3>BASIC PLAN</h3>
-					<p><label>$</label> <span>25 </span>/ mo</p>
+					<h3>Комплексное
+					бухгалтерское
+					обслуживание</h3>
+					<p>от <label>1000</label> &#8399;</p>
 				</div>
 				<ul>
-					<li>60 Users</li>
-					<li>Unlimited Forums</li>
-					<li>Unlimited Reports</li>
-					<li>3,000 Entries per Month</li>
-					<li>200 MB Storage</li>
+					<li></li>
 				</ul>
-				<div class="started"><a href="#">GET STARTED NOW</a></div>
+				<div class="started"><a href="#">Подробнее</a></div>
 				<div class="para">
-					<p>30 DAYS FREE TRIAL</p>
+					<p></p>
 				</div>
 			</div>
 			<div class="pricing-grid">
 				<div class="basic-plan">
-					<h3>BASIC PLAN</h3>
-					<p><label>$</label> <span>25 </span>/ mo</p>
+					<h3>Бухгалтерское
+					сопровождение</h3>
+					<p>от <label>1000</label> &#8399;</p>
 				</div>
 				<ul>
-					<li>60 Users</li>
-					<li>Unlimited Forums</li>
-					<li>Unlimited Reports</li>
-					<li>3,000 Entries per Month</li>
-					<li>200 MB Storage</li>
+					<li></li>
 				</ul>
-				<div class="started"><a href="#">GET STARTED NOW</a></div>
+				<div class="started"><a href="#">Подробнее</a></div>
 				<div class="para">
-					<p>30 DAYS FREE TRIAL</p>
+					<p></p>
 				</div>
 			</div>
 			<div class="pricing-grid">
 				<div class="basic-plan">
-					<h3>BASIC PLAN</h3>
-					<p><label>$</label> <span>25 </span>/ mo</p>
+					<h3>Постановка
+					бухгалтерского
+					учёта</h3>
+					<p>от <label>1000</label> &#8399;</p>
 				</div>
 				<ul>
-					<li>60 Users</li>
-					<li>Unlimited Forums</li>
-					<li>Unlimited Reports</li>
-					<li>3,000 Entries per Month</li>
-					<li>200 MB Storage</li>
+					<li></li>
 				</ul>
-				<div class="started"><a href="#">GET STARTED NOW</a></div>
+				<div class="started"><a href="#">Подробнее</a></div>
 				<div class="para">
-					<p>30 DAYS FREE TRIAL</p>
+					<p></p>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+		<br />
+		<div class="pricing-grids">
+			<div class="pricing-grid">
+				<div class="basic-plan">
+					<h3>Восстановление
+					бухгалтерского и
+					налогового учёта</h3>
+					<p>от <label>1000</label> &#8399;</p>
+				</div>
+				<ul>
+					<li></li>
+				</ul>
+				<div class="started"><a href="#">Подробнее</a></div>
+				<div class="para">
+					<p></p>
+				</div>
+			</div>
+			<div class="pricing-grid">
+				<div class="basic-plan">
+					<h3>Сопровождение
+					судебных споров</h3>
+					<p>от <label>1000</label> &#8399;</p>
+				</div>
+				<ul>
+					<li></li>
+				</ul>
+				<div class="started"><a href="#">Подробнее</a></div>
+				<div class="para">
+					<p></p>
+				</div>
+			</div>
+			<div class="pricing-grid">
+				<div class="basic-plan">
+					<h3>Регистрация
+					ООО или ИП</h3>
+					<p>от <label>1000</label> &#8399;</p>
+				</div>
+				<ul>
+					<li></li>
+				</ul>
+				<div class="started"><a href="#">Подробнее</a></div>
+				<div class="para">
+					<p></p>
 				</div>
 			</div>
 			<div class="clearfix"></div>
@@ -319,86 +367,71 @@
 </div>
 <!-- //pricing-table -->
 <!-- our-team -->
-<div id="our-team" class="our-team">
+<div id="atv-feedbacks" class="our-team">
 	<div class="container">
 		<div class="team-info text-center">
-			<h2><span>OUR</span> TEAM</h2>
-			<h3><i>talented Folks</i></h3>
+			<h2><span>Отзывы</span> наших клиентов</h2>
+			<h3></h3>
 			<div class="underline">
-				<img src="images/k.png" alt=" "/>
+				
 			</div>
-			<p>Lorem ipsum dolor amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi suspendisse</p>
 		</div>
-		<div class="team-grids">
-			<div class="col-md-3 team-grid">
-				<div class="team-grid-image">
-					<img src="images/8.jpg" alt=" "/>
-					<ul>
-						<li><a class="fb" href="#"></a></li>
-						<li><a class="twit" href="#"></a></li>
-						<li><a class="linkin" href="#"></a></li>
-						<li><a class="be" href="#"></a></li>
-					</ul>
-				</div>
-				<div class="team-grid-info">
-					<h3>PATRICIA MEDINA</h3>
-					<p><span class="highlight">Senior Designer</span></p>
-					<p>Imperdiet nulla sem fringilla, purus enim amet, nascetur faucibus, adipiscing neque ut bibendum, 
-					at felis nec in.</p>
-				</div>
-			</div>
-			<div class="col-md-3 team-grid">
-				<div class="team-grid-image">
-					<img src="images/8.jpg" alt=" "/>
-					<ul>
-						<li><a class="fb" href="#"></a></li>
-						<li><a class="twit" href="#"></a></li>
-						<li><a class="linkin" href="#"></a></li>
-						<li><a class="be" href="#"></a></li>
-					</ul>
-				</div>
-				<div class="team-grid-info">
-					<h3>PATRICIA MEDINA</h3>
-					<p><span class="highlight">Senior Designer</span></p>
-					<p>Imperdiet nulla sem fringilla, purus enim amet, nascetur faucibus, adipiscing neque ut bibendum, 
-					at felis nec in.</p>
-				</div>
-			</div>
-			<div class="col-md-3 team-grid">
-				<div class="team-grid-image">
-					<img src="images/8.jpg" alt=" "/>
-					<ul>
-						<li><a class="fb" href="#"></a></li>
-						<li><a class="twit" href="#"></a></li>
-						<li><a class="linkin" href="#"></a></li>
-						<li><a class="be" href="#"></a></li>
-					</ul>
-				</div>
-				<div class="team-grid-info">
-					<h3>PATRICIA MEDINA</h3>
-					<p><span class="highlight">Senior Designer</span></p>
-					<p>Imperdiet nulla sem fringilla, purus enim amet, nascetur faucibus, adipiscing neque ut bibendum, 
-					at felis nec in.</p>
-				</div>
-			</div>
-			<div class="col-md-3 team-grid">
-				<div class="team-grid-image">
-					<img src="images/8.jpg" alt=" "/>
-					<ul>
-						<li><a class="fb" href="#"></a></li>
-						<li><a class="twit" href="#"></a></li>
-						<li><a class="linkin" href="#"></a></li>
-						<li><a class="be" href="#"></a></li>
-					</ul>
-				</div>
-				<div class="team-grid-info">
-					<h3>PATRICIA MEDINA</h3>
-					<p><span class="highlight">Senior Designer</span></p>
-					<p>Imperdiet nulla sem fringilla, purus enim amet, nascetur faucibus, adipiscing neque ut bibendum, 
-					at felis nec in.</p>
+		<br />
+
+		<div class="feedback-container">
+			<a href="#" class="jcarousel-control-prev inactive" data-jcarouselcontrol="true"><img src="images/left-arrow.png" /></a>
+			<a href="#" class="jcarousel-control-next" data-jcarouselcontrol="true"><img src="images/left-arrow.png" /></a>
+			
+			<div class="feedback-carousel">
+
+				<div class="team-grids">
+					<div class="col-md-3 team-grid">
+						<div class="team-grid-image">
+							<img src="images/88.jpg" alt=" "/>
+							Фото
+						</div>
+						<div class="team-grid-info">
+							<h3>Фамилия Имя</h3>
+							<p><span class="highlight">Компания</span></p>
+							<p class="team-grid-feedback">Текст отзыва</p>
+						</div>
+					</div>
+					<div class="col-md-3 team-grid">
+						<div class="team-grid-image">
+							<img src="images/88.jpg" alt=" "/>
+							Фото
+						</div>
+						<div class="team-grid-info">
+							<h3>Фамилия Имя</h3>
+							<p><span class="highlight">Компания</span></p>
+							<p class="team-grid-feedback">Текст отзыва</p>
+						</div>
+					</div>
+					<div class="col-md-3 team-grid">
+						<div class="team-grid-image">
+							<img src="images/88.jpg" alt=" "/>
+							Фото
+						</div>
+						<div class="team-grid-info">
+							<h3>Фамилия Имя</h3>
+							<p><span class="highlight">Компания</span></p>
+							<p class="team-grid-feedback">Текст отзыва</p>
+						</div>
+					</div>
+					<div class="col-md-3 team-grid">
+						<div class="team-grid-image">
+							<img src="images/88.jpg" alt=" "/>
+							Фото
+						</div>
+						<div class="team-grid-info">
+							<h3>Фамилия Имя</h3>
+							<p><span class="highlight">Компания</span></p>
+							<p class="team-grid-feedback">Текст отзыва</p>
+						</div>
+					</div>
+					<!-- <div class="clearfix"></div> -->
 				</div>
 			</div>
-			<div class="clearfix"></div>
 		</div>
 	</div>
 </div>
@@ -408,7 +441,7 @@
 <div id="get-in-touch" class="get-in-touch">
 	<div class="container">
 		<div class="get-info text-center">
-			<h2><span class="highlight">Как нас найти</span></h2>
+			<h2><span class="highlight">Как</span> нас найти</h2>
 			<p></p>
 		</div>
 	</div>
@@ -576,8 +609,62 @@
 
 		$('.atv_phone').mask('+0 (000) 000-00-00');
 		$('.atv_phone').on('click', function() {
-			$('.atv_phone').val('+7 ');
+			if ( $('.atv_phone').val() == "" )
+				$('.atv_phone').val('+7 ');
 		});
+	
+
+		/* jcarousel */
+			$('.feedback-carousel').jcarousel({
+				animation: 'slow',
+				wrap: 'both',
+			});
+
+			/*
+			 Prev control initialization
+			 */
+			$('.jcarousel-control-prev')
+				.on('jcarouselcontrol:active', function() {
+					$(this).removeClass('inactive');
+				})
+				.on('jcarouselcontrol:inactive', function() {
+					$(this).addClass('inactive');
+				})
+				.jcarouselControl({
+					// Options go here
+					target: '-=1'
+				});
+
+			/*
+			 Next control initialization
+			 */
+			$('.jcarousel-control-next')
+				.on('jcarouselcontrol:active', function() {
+					$(this).removeClass('inactive');
+				})
+				.on('jcarouselcontrol:inactive', function() {
+					$(this).addClass('inactive');
+				})
+				.jcarouselControl({
+					// Options go here
+					target: '+=1'
+				});
+
+			$('.jcarousel-control-prev').hover(function(){
+				$('.jcarousel-control-prev').animate({"margin-right":"7px"}, 'fast');
+			},
+			function(){
+				$('.jcarousel-control-prev').animate({"margin-right":"0px"}, 'fast');
+			});
+
+			$('.jcarousel-control-next').hover(function(){
+				$('.jcarousel-control-next').animate({"margin-left":"7px"}, 'fast');
+			},
+			function(){
+				$('.jcarousel-control-next').animate({"margin-left":"0px"}, 'fast');
+			});
+		/* end jcarousel */
+
 	});
 
 </script>
