adb76be8106609f51e747bc18c1edcfb8dad8aa1
Igor Popov <ihptru@gmail.com>
2016-02-27 09:22:12 +0000

    update adsense

--- index.php
+++ index.php
@@ -712,9 +712,9 @@
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<!-- atv61.ru -->
 	<ins class="adsbygoogle"
-	     style="display:inline-block;width:980px;height:120px"
-	     data-ad-client="ca-pub-9777209289183863"
-	     data-ad-slot="3657807833"></ins>
+	     style="display:inline-block;width:728px;height:90px"
+	     data-ad-client="ca-pub-1502331739186135"
+	     data-ad-slot="7071459804"></ins>
 	<script>
 	(adsbygoogle = window.adsbygoogle || []).push({});
 	</script>
